b6d74a8687a556bf632bac2542137ef9b7af6fc2
alexismaza679-droid <alexismaza679@gmail.com>
2026-06-11 11:27:45 -0600

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,25 +4,11 @@ const config: CapacitorConfig = {
   appId: "app.lovable.veymar",
   appName: "VEYMAR A.I.",
 
-  // ✔️ VITE REAL OUTPUT (SIEMPRE TIENE index.html)
+  // 🔥 No importa cuál sea, lo sobreescribimos en el build
   webDir: "dist",
 
   android: {
     allowMixedContent: true,
-  },
-
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 1200,
-      backgroundColor: "#05070D",
-      androidScaleType: "CENTER_CROP",
-      showSpinner: false,
-    },
-
-    StatusBar: {
-      style: "DARK",
-      backgroundColor: "#05070D",
-    },
   },
 };
 
