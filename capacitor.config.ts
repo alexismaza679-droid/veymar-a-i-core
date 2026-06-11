ca9e4bb48d2e12daa3e6e058962b055015ae5f53
alexismaza679-droid <alexismaza679@gmail.com>
2026-06-11 09:49:24 -0600

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,15 +4,11 @@ const config: CapacitorConfig = {
   appId: "app.lovable.veymar",
   appName: "VEYMAR A.I.",
   webDir: "dist",
-  server: {
-    // Hot-reload desde el preview de Lovable mientras desarrollas.
-    // Para una APK 100% offline, comenta este bloque y vuelve a compilar.
-    url: "https://80307ab0-68a2-4e5e-9c7e-1c8d378c0022.lovableproject.com?forceHideBadge=true",
-    cleartext: true,
-  },
+
   android: {
     allowMixedContent: true,
   },
+
   plugins: {
     SplashScreen: {
       launchShowDuration: 1500,
@@ -20,6 +16,7 @@ const config: CapacitorConfig = {
       androidScaleType: "CENTER_CROP",
       showSpinner: false,
     },
+
     StatusBar: {
       style: "DARK",
       backgroundColor: "#05070D",
