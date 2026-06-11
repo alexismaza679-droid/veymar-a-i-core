210266ff336f902a33d63c1a09f4088bc706d32f
alexismaza679-droid <alexismaza679@gmail.com>
2026-06-11 10:02:40 -0600

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "app.lovable.veymar",
   appName: "VEYMAR A.I.",
-  webDir: "dist",
+
+  webDir: ".output/public",
 
   android: {
     allowMixedContent: true,
